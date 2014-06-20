51233e7759eccefb3b3efb3f5760fef0c57b7562
Boris Stanojevic <stanojevic.boris@gmail.com>
2014-06-20 11:07:41 +0200

    osgi client config for xds-iti42 bridge

---
+++ tutorials/osgi/ihe/route-xds/src/main/groovy/org/openehealth/ipf/tutorials/osgi/ihe/xds/iti42/route/GroovyRouteBuilder.groovy
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2013 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.openehealth.ipf.tutorials.osgi.ihe.xds.iti42.route
+
+import org.apache.camel.spring.SpringRouteBuilder
+import static org.openehealth.ipf.platform.camel.ihe.xds.XdsCamelValidators.iti42RequestValidator
+
+/**
+ * @author Boris Stanojevic
+ */
+public class GroovyRouteBuilder extends SpringRouteBuilder {
+
+    @Override
+    public void configure() throws Exception {
+        from('xds-iti42:xds-iti42-service1')
+//            .process(iti42RequestValidator())
+            .to("xds-iti42://ihexds.nist.gov:12080/tf6/services/xdsregistryb")
+
+    }
+
+}
